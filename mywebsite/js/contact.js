fb5bdab6ac0876a5839079ad5aada16993e5fade
talist225 <talist225@gmail.com>
2022-07-11 11:31:01 +0300

    Update contact.js

--- mywebsite/js/contact.js
+++ mywebsite/js/contact.js
@@ -2,7 +2,7 @@ function sendEmail() {
   Email.send({
     Host: "smtp.gmail.com",
     Username: "talist225@gmail.com",
-    Password: "password",
+    Password: "ykhgecvnkl225",
     To: "theleangen@gmail.com",
     From: document.getElementById("email").value,
     Subject: "New Contact from Enquiry",
